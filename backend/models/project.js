643ba4f40065b126f23082ad2fe53ae93b54c75a
marcopuga49 <47088598+marcopuga49@users.noreply.github.com>
2019-10-09 09:42:36 -0500

    Backend de la aplicación angular
    Se sube la parte de la base de datos que esta ligada a los campos del formulario.
    Se realiza para que la aplicación este guardando todo tipo de informacion ingresada en el formulario.

---
+++ backend/models/project.js
@@ -0,0 +1,24 @@
+'use strict'
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var ProjectSchema = Schema({
+	name: String,
+    descripcion: String,
+    category: String,
+    year: Number,
+    langs: String,
+    image: String
+});
+
+
+module.exports = mongoose.model('Project', ProjectSchema);
+
+
+
+
+
+
+
+
